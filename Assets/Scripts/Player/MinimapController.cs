598c11156cef38a3ff21d0def979dd383839d7e6
CharcoalZz <chamodsilva@gmail.com>
2020-03-04 00:11:52 +0800

    V2 Added camera pan control, Minimap

---
+++ Assets/Scripts/Player/MinimapController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 posOffset;
+
+    void Update()
+    {
+        transform.position = target.position + posOffset;
+    } 
+}
---
+++ Assets/Scripts/Player/NewCameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCameraController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+    public float panBorderY = 200f;
+    public float panBorderX = 400f;
+    public Vector2 panLimit;
+
+
+    void Update()
+    {
+        Vector3 pos = transform.localPosition;
+
+        if(Input.GetKey(KeyCode.Mouse2) && Input.mousePosition.y >= Screen.height - panBorderY)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Mouse2) && Input.mousePosition.y <= panBorderY)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Mouse2) && Input.mousePosition.x >= Screen.width - panBorderX)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Mouse2) && Input.mousePosition.x <= panBorderX)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+
+        transform.localPosition = pos;
+
+    }
+}
